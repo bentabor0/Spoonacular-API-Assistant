47eaaffe812fad773090c0ddabcc54316d5ac42f
Ben Tabor <bentabor6215@gmail.com>
2022-09-12 15:51:52 -0500

    Revert "Merge branch 'test2'"
    This reverts commit 30bf03dd6ff2b0a3532f7f1667c887ab118dd5f2, reversing
    changes made to e1f192edbc12976b7805768c7fc0d6baa445bbb7.

---
+++ 12.2Assignment/DTO/UserDTO.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._2Assignment
+{
+    public class UserDTO
+    {
+        [JsonProperty("username")]
+        public string username { get; set; }
+
+        [JsonProperty("firstname")]
+        public string firstname { get; set; }
+
+        [JsonProperty("lastname")]
+        public string lastname { get; set; }
+
+        [JsonProperty("email")]
+        public string email { get; set; } 
+    }
+}
---
+++ 12.2Assignment/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._2Assignment
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Summary { get; set; }
+
+        public string SourceUrl { get; set; }
+    }
+}
